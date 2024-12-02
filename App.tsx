2f5b46868ffdc128da8fbf275eeabaea28f82d12
logg1s <lrng159@gmail.com>
2024-12-02 09:24:27 +0700

    4.19 Flex-box system

--- App.tsx
+++ App.tsx
@@ -1,26 +1,23 @@
-import { StyleSheet, Text, View } from "react-native";
-import { Profiles } from "./components/Profiles/Profiles";
+import { Text, View } from "react-native";
 import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
+import { s } from "./App.style";
 
 export default function App() {
   return (
     <SafeAreaProvider>
-      <SafeAreaView>
-        <Text>App</Text>
-        <View>
-          <Text style={style.text}>Hello</Text>
+      <SafeAreaView style={{ display: "flex", flex: 1 }}>
+        <View style={s.box1}>
+          <View
+            style={{ height: 50, width: 100, backgroundColor: "red" }}
+          ></View>
+          <Text style={s.text}>Khá là hay ho</Text>
+          <Text style={s.text}>Khá là hay ho</Text>
+          <Text style={s.text}>Khá là hay ho</Text>
+          <Text style={s.text}>Khá là hay ho</Text>
         </View>
-        <Profiles />
+        <View style={s.box2} />
+        <View style={s.box3} />
       </SafeAreaView>
     </SafeAreaProvider>
   );
 }
-
-
-const style = StyleSheet.create({
-  text: {
-    color: "blue",
-    fontSize: 200,
-    direction: "rtl"
-  }
-})
