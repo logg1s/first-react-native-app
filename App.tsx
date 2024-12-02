2f1b7c8f8da51cf112af2e3177c79b505371a92c
logg1s <lrng159@gmail.com>
2024-12-02 09:24:27 +0700

    4.18 Styling

--- App.tsx
+++ App.tsx
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
+import { Profiles } from "./components/Profiles/Profiles";
+import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView>
+        <Text>App</Text>
+        <View>
+          <Text style={style.text}>Hello</Text>
+        </View>
+        <Profiles />
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+const style = StyleSheet.create({
+  text: {
+    color: "blue",
+    fontSize: 200,
+    direction: "rtl"
+  }
+})
---
+++ components/Profiles/Profiles.tsx
@@ -0,0 +1,10 @@
+import { Text, TouchableOpacity } from "react-native";
+
+export function Profiles() {
+  return <>
+    <Text>Profiles</Text>
+    <TouchableOpacity>
+        <Text>Click me !</Text>
+    </TouchableOpacity>
+  </>
+}
