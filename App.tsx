02b3da007153f0384c0b2f65be6bfc7f05619a94
logg1s <lrng159@gmail.com>
2024-12-02 20:35:50 +0700

    4.29 Detect Platform

--- App.tsx
+++ App.tsx
@@ -1,39 +1,22 @@
+import { Platform, Text } from "react-native";
 import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
-import { Profil } from "./components/Profil/Profil";
-import { useState } from "react";
-import { Linking, Text } from "react-native";
-
-export const socialMediaLink = {
-  twitter: "https://twitter.com",
-  linkedin: "https://linkedin.com",
-  facebook: "https://facebook.com",
-  instagram: "https://instagram.com",
-  github: "https://github.com",
-};
-
 export default function App() {
-  const [clickCount, setClickCount] = useState(0);
-
-  function onClickTitle() {
-    setClickCount((prevClickCount) => prevClickCount + 1);
-  }
-
-  function onPressSocialMediaIcon(socialMediaUrl: string) {
-    Linking.openURL(socialMediaUrl);
-  }
-
   return (
     <SafeAreaProvider>
       <SafeAreaView style={{ flex: 1, justifyContent: "center", padding: 20 }}>
-        <Profil
-          age={24}
-          firstName="Long"
-          lastName="Nguyễn"
-          isOpenToWork={true}
-          onPressTitle={onClickTitle}
-          onPressSocialMediaIcon={onPressSocialMediaIcon}
-        />
-        <Text>You have clicked {clickCount} times on title</Text>
+        <Text style={{ fontSize: 36, fontWeight: "bold" }}>
+          {Platform.isTV ? "Hello TV!" : "Hello Mobile!"}
+        </Text>
+        <Text style={{ fontSize: 36, fontWeight: "bold" }}>
+          {Platform.OS === "android"
+            ? "You are using Android"
+            : "You are using iOS"}
+        </Text>
+        <Text style={{ fontSize: 36, fontWeight: "bold" }}>
+          {Platform.Version === 31
+            ? "You are using Android 12"
+            : "You are using Android 11 or below"}
+        </Text>
       </SafeAreaView>
     </SafeAreaProvider>
   );
